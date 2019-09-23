07a1f434b18995c8edb3aa3eb46be3b23cf0b0b1
dasevo <dasek.vojtech@gmail.com>
2019-09-23 17:29:32 +0200

    porovnání url

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ http.createServer((req, res) => {
     if (req.url == "/") {
         citac++;
     }
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body> poet volání: " + citac + "</body></html>");
+    if (req.url == "/jinastranka") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body> jina strana</body></html>");
+    } else {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body> pocet volání: " + citac + "</body></html>");
+    }
 }).listen(2000);
