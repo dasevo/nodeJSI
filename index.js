26b341f4f182635563e555f064429edc1bcbcbe1
dasevo <dasek.vojtech@gmail.com>
2019-10-07 17:44:34 +0200

    Sv8tkz

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 const http = require("http");
 const dateFormat = require("dateformat");
+const fs = require("fs");
 
 const DNY_V_TYDNU = ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"];
 
+const SVATKY = new Array();
+SVATKY[1] = ["", 'Nový rok', 'Karina', 'Radmila', 'Diana', 'Dalimil', 'Tři králové', 'Vilma', 'Čestmír', 'Vladan', 'Břetislav', 'Bohdana', 'Pravoslav', 'Edita', 'Radovan', 'Alice', 'Ctirad', 'Drahoslav', 'Vladislav', 'Doubravka', 'Ilona', 'Běla', 'Slavomír', 'Zdeněk', 'Milena', 'Miloš', 'Zora', 'Ingrid', 'Otýlie', 'Zdislava', 'Robin', 'Marika'];
+SVATKY[2] = ["", 'Hynek', 'Nela a Hromnice', 'Blažej', 'Jarmila', 'Dobromila', 'Vanda', 'Veronika', 'Milada', 'Apolena', 'Mojmír', 'Božena', 'Slavěna', 'Věnceslav', 'Valentýn', 'Jiřina', 'Ljuba', 'Miloslava', 'Gizela', 'Patrik', 'Oldřich', 'Lenka', 'Petr', 'Svatopluk', 'Matěj', 'Liliana', 'Dorota', 'Alexandr', 'Lumír', 'Horymír'];
+SVATKY[3] = ["", 'Bedřich', 'Anežka', 'Kamil', 'Stela', 'Kazimír', 'Miroslav', 'Tomáš', 'Gabriela', 'Františka', 'Viktorie', 'Anděla', 'Řehoř', 'Růžena', 'Rút a Matylda', 'Ida', 'Elena a Herbert', 'Vlastimil', 'Eduard', 'Josef', 'Světlana', 'Radek', 'Leona', 'Ivona', 'Gabriel', 'Marián', 'Emanuel', 'Dita', 'Soňa', 'Taťána', 'Arnošt', 'Kvido'];
+SVATKY[4] = ["", 'Hugo', 'Erika', 'Richard', 'Ivana', 'Miroslava', 'Vendula', 'Heřman a Hermína', 'Ema', 'Dušan', 'Darja', 'Izabela', 'Julius', 'Aleš', 'Vincenc', 'Anastázie', 'Irena', 'Rudolf', 'Valérie', 'Rostislav', 'Marcela', 'Alexandra', 'Evžénie', 'Vojtěch', 'Jiří', 'Marek', 'Oto', 'Jaroslav', 'Vlastislav', 'Robert', 'Blahoslav'];
+SVATKY[5] = ["", 'Svátek práce', 'Zikmund', 'Alexej', 'Květoslav', 'Klaudie', 'Radoslav', 'Stanislav', 'Statní svátek - Ukončení II. světové války', 'Ctibor', 'Blažena', 'Svatava', 'Pankrác', 'Servác', 'Bonifác', 'Žofie', 'Přemysl', 'Aneta', 'Nataša', 'Ivo', 'Zbyšek', 'Monika', 'Emil', 'Vladimír', 'Jana', 'Viola', 'Filip', 'Valdemar', 'Vilém', 'Maxim', 'Ferdinand', 'Kamila'];
+SVATKY[6] = ["", 'Laura', 'Jarmil', 'Tamara', 'Dalibor', 'Dobroslav', 'Norbert', 'Iveta', 'Medard', 'Stanislava', 'Gita', 'Bruno', 'Antonie', 'Antonín', 'Roland', 'Vít', 'Zbyněk', 'Adolf', 'Milan', 'Leoš', 'Květa', 'Alois', 'Pavla', 'Zdeňka', 'Jan', 'Ivan', 'Adriana', 'Ladislav', 'Lubomír', 'Petr a Pavel', 'Šárka'];
+SVATKY[7] = ["", 'Jaroslava', 'Patricie', 'Radomír', 'Prokop', 'Státní svátek , Cyril a Metoděj', 'Státní svátek , Mistr Jan Hus', 'Bohuslava', 'Nora', 'Drahoslava', 'Libuše a Amálie', 'Olga', 'Bořek', 'Markéta', 'Karolína', 'Jindřich', 'Luboš', 'Martina', 'Drahomíra', 'Čeněk', 'Ilja', 'Vítězslav', 'Magdaléna', 'Libor', 'Kristýna', 'Jakub', 'Anna', 'Věroslav', 'Viktor', 'Marta', 'Bořivoj', 'Ignác'];
+SVATKY[8] = ["", 'Oskar', 'Gustav', 'Miluše', 'Dominik', 'Kristián', 'Oldřiška', 'Lada', 'Soběslav', 'Roman', 'Vavřinec', 'Zuzana', 'Klára', 'Alena', 'Alan', 'Hana', 'Jáchym', 'Petra', 'Helena', 'Ludvík', 'Bernard', 'Johana', 'Bohuslav', 'Sandra', 'Bartoloměj', 'Radim', 'Luděk', 'Otakar', 'Augustýn', 'Evelína', 'Vladěna', 'Pavlína'];
+SVATKY[9] = ["", 'Linda a Samuel', 'Adéla', 'Bronislav', 'Jindřiška', 'Boris', 'Boleslav', 'Regína', 'Mariana', 'Daniela', 'Irma', 'Denisa', 'Marie', 'Lubor', 'Radka', 'Jolana', 'Ludmila', 'Naděžda', 'Kryštof', 'Zita', 'Oleg', 'Matouš', 'Darina', 'Berta', 'Jaromír', 'Zlata', 'Andrea', 'Jonáš', 'Václav', 'Michal', 'Jeroným'];
+SVATKY[10] = ["", 'Igor', 'Olívie a Oliver', 'Bohumil', 'František', 'Eliška', 'Hanuš', 'Justýna', 'Věra', 'Štefan a Sára', 'Marina', 'Andrej', 'Marcel', 'Renáta', 'Agáta', 'Tereza', 'Havel', 'Hedvika', 'Lukáš', 'Michaela', 'Vendelín', 'Brigita', 'Sabina', 'Teodor', 'Nina', 'Beáta', 'Erik', 'Šarlota a Zoe', 'Statní svátek - Vznik Československa', 'Silvie', 'Tadeáš', 'Štěpánka'];
+SVATKY[11] = ["", 'Felix', 'Památka zesnulých', 'Hubert', 'Karel', 'Miriam', 'Liběna', 'Saskie', 'Bohumír', 'Bohdan', 'Evžen', 'Martin', 'Benedikt', 'Tibor', 'Sáva', 'Leopold', 'Otmar', 'Mahulena', 'Romana', 'Alžběta', 'Nikola', 'Albert', 'Cecílie', 'Klement', 'Emílie', 'Kateřina', 'Artur', 'Xenie', 'René', 'Zina', 'Ondřej'];
+SVATKY[12] = ["", 'Iva', 'Blanka', 'Svatoslav', 'Barbora', 'Jitka', 'Mikuláš', 'Ambrož', 'Květoslava', 'Vratislav', 'Julie', 'Dana', 'Simona', 'Lucie', 'Lýdie', 'Radana', 'Albína', 'Daniel', 'Miloslav', 'Ester', 'Dagmar', 'Natálie', 'Šimon', 'Vlasta', 'Adam a Eva , Štědrý den', '1. svátek vánoční', 'Štěpán , 2. svátek vánoční', 'Žaneta', 'Bohumila', 'Judita', 'David', 'Silvestr'];
+
+
 let citac = 0;
+
+function processStaticFiles(res, fileName) {
+    fileName = fileName.substr(1);
+    console.log(fileName);
+    let contentType = "text/html";
+
+    if (fileName.endsWith(".png")) {
+        contentType = "image/png";
+    } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+        //console.log("obrazek");
+        contentType = "image/jpeg";
+    }
+
+    console.log(contentType);
+
+    if (fs.existsSync(fileName)) {
+        fs.readFile(fileName, function (err, data) {
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(data);
+            res.end();
+        });
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+}
 
 http.createServer((req, res) => {
     if (req.url === "/") {
         citac++;
+        processStaticFiles(res, "/index.html");
+        return;
+    }
+    if (req.url.length - req.url.lastIndexOf(".") < 6) {
+        processStaticFiles(res, req.url);
+        return;
     }
     if (req.url === "/jinastranka") {
         res.writeHead(200, {"Content-type": "text/html"});
@@ -22,17 +70,68 @@ http.createServer((req, res) => {
     } else if (req.url === "/denvtydnu") {
         res.writeHead(200, {
             "Content-type": "application/json",
-            "Access-Control-Allow-Origin":"*"
+            "Access-Control-Allow-Origin": "*"
         });
-        let d = new Date()
-        let obj = {}
+        let d = new Date();
+        let obj = {};
         obj.datum = d;
         obj.cisloDne = d.getDay(); // nedele = 0
-        obj.datumCesky = d.getDate() + "." + (d.getMonth()+1) + "." + d.getFullYear(); // leden = 0
+        obj.datumCesky = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear(); // leden = 0
         obj.formatovano = dateFormat(d, "dd.mm.yy");
         obj.casCesky = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
         obj.denVTydnu = DNY_V_TYDNU[obj.cisloDne];
         obj.casUTC = d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds();
+        res.end(JSON.stringify(obj));
+    } else if (req.url === "/svatky") {
+        res.writeHead(200, {
+            "Content-type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        });
+        let d = new Date();
+        let obj = {};
+        obj.den = d.getDate();
+        obj.mesic = d.getMonth() + 1;
+        obj.rok = d.getFullYear();
+        obj.jmeno = SVATKY[obj.mesic][obj.den];
+        obj.zitraRok = obj.rok;
+        let r = obj.rok;
+        let m = obj.mesic;
+        let e = obj.d;
+        let zitraDen = obj.den + 1;
+        let zitraMesic = obj.mesic;
+        if(m==1, m==3, m==5, m==7, m==8, m==10, m==12) {
+            if(e==31) {
+                zitraDen = 1;
+                zitraMesic++;
+            }
+        }
+        if(m==4, m==6, m==9, m==11) {
+            if(e==30) {
+                zitraDen = 1;
+                zitraMesic++;
+            }
+        }
+        if(m==2) {
+            if(r % 4 == 0 && r%100 != 0) {
+                if (e == 29) {
+                    zitraDen = 1;
+                    zitraMesic++;
+                }
+            }
+            else{
+                if(e==28) {
+                    zitraDen = 1;
+                    zitraMesic++;
+                }
+            }
+        }
+        if(zitraMesic==13) {
+            zitraMesic = 1;
+            obj.zitraRok = obj.rok + 1;
+        }
+        obj.zitraDen = zitraDen;
+        obj.zitraMesic = zitraMesic;
+        obj.jmenoZitra = SVATKY[obj.zitraMesic][obj.zitraDen];
         res.end(JSON.stringify(obj));
     } else {
         res.writeHead(200, {"Content-type": "text/html"});
